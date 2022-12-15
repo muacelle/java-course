b08ef3fe54d2a7ad173d18c027dd95ef91ec3965
Marcelle Pernet <mevpernet@gmail.com>
2022-12-15 12:18:44 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        // REFERENCE TYPES
 
         class Person {
             String name;
@@ -16,5 +22,55 @@ public class Main {
 
         alex.name = "Alexander";
         System.out.print(marian.name);
+
+        // ARRAYS
+
+        int [] numbers = new int[2];
+        System.out.println(Arrays.toString(numbers)); // [0, 0]
+
+        int [] specific = {5, 6, 7};
+        System.out.println(Arrays.toString(specific)); // [5, 6, 7]
+
+        boolean [] boos = new boolean[3];
+        System.out.println(Arrays.toString(boos)); // [false, false, false]
+
+        String [] strs = new String[2];
+        System.out.println(Arrays.toString(strs)); // [null, null]
+
+        // USER INPUT
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName);
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        int yearBorn = LocalDate.now().minusYears(age).getYear();
+        System.out.println("You were born in " + yearBorn);
+
+        // OBJECTS
+
+        Lens lensOne = new Lens("Sony", "85mm", true);
+        Lens lensTwo = new Lens("Canon", "24-70mm", false);
+
+        System.out.println(lensOne);
+    }
+
+    // CLASSES
+
+    static class Lens {
+        String brand;
+        String focalLength;
+        boolean isPrime;
+
+        // CONSTRUCTORS
+
+        Lens(String brand, String focalLength, boolean isPrime) {
+            this.brand = brand;
+            this.focalLength = focalLength;
+            this.isPrime = isPrime;
+        }
     }
 }
